6082901503895dc640eb85460fedce193b8dbe88
treyh23 <forrest@contempows.com>
2018-01-10 15:49:31 -0800

    question 7 commit

--- js/app.js
+++ js/app.js
@@ -3,33 +3,33 @@
 var questionOne = prompt('is my hair color blue? Repsond with Y or N').toUpperCase();
 
 if(questionOne === 'Y' ) {
-    alert('That is unfortunately not true');
+  alert('That is unfortunately not true');
 } else if (questionOne === 'N'){
-    alert('You are correct :)');    
+  alert('You are correct :)');  
 } else {
-    alert('you were suppose to answer using Y or N');
+  alert('you were suppose to answer using Y or N');
 }
 console.log('the users answer is ' + questionOne);
 
 var questionTwo = prompt('is pizza my favorite italian food? - Remember, use Y or N for your answers').toUpperCase();
 
 if(questionTwo === 'Y') {
-    alert('You are correct! Pizza is my favorite Italian food');
+  alert('You are correct! Pizza is my favorite Italian food');
 } else if (questionTwo === 'N') {
-    alert('Unfortunate guess - Pizza is my favorite Italian food');
+  alert('Unfortunate guess - Pizza is my favorite Italian food');
 } else {
-    alert('You entered the wrong answer! Try again');
+  alert('You entered the wrong answer! Try again');
 };
 console.log(questionTwo + ' is his favorite Italian food!')
 
 var questionThree = prompt('is my name Forrest? - Also, use Y or N for your answer').toUpperCase();
 
 if(questionThree === 'Y') {
-    alert('You are correct! & It\'s spelled like Forrest Gump');
+  alert('You are correct! & It\'s spelled like Forrest Gump');
 } else if (questionThree = 'N') {
-    alert('Wrong answer :(');
+  alert('Wrong answer :(');
 } else {
-    alert('did you enter the right type of answer??');
+  alert('did you enter the right type of answer??');
 };
 
 console.log('the gentleman that wrote this program, his name is ' + questionThree)
@@ -37,21 +37,42 @@ console.log('the gentleman that wrote this program, his name is ' + questionThre
 var questionFour = prompt('Were you born in Florida? - Remember, answer using Y or N').toLocaleUpperCase();
 
 if(questionFour === 'Y') { 
-    alert('You are incorrect');
+  alert('You are incorrect');
 } else if (questionFour === 'N') {
-    alert('You are right! I was born in Philadelphia');
+  alert('You are right! I was born in Philadelphia');
 } else {
-    alert('You didnt answer in the right format :/');
+  alert('You didnt answer in the right format :/');
 };
 console.log('Yes, he was born in ' + questionFour);
 
 var questionFive = prompt('Do you like entrepreneurship? - Remember, use Y or N to answer').toUpperCase();
 
 if(questionFive === 'Y') {
-    alert('Yes, I don\'t just like entrepreneurship, i\'m nerdy and obssesed with it.');
+  alert('Yes, I don\'t just like entrepreneurship, i\'m nerdy and obssesed with it.');
 } else if (questionFive === 'N') {
-    alert('oh whoa, you need to get to know me some more. This is the wrong answer');
+  alert('oh whoa, you need to get to know me some more. This is the wrong answer');
 } else {
-    alert('did you write your answer in the right format?');
+  alert('did you write your answer in the right format?');
 };
 console.log('If you couldn\'t tell ' + questionFive + ' he does like entrepreneurship');
+
+var careerLength = 7;
+
+var lengthGuess;
+for(var i = 0; i < 4; i++) {
+
+  lengthGuess = prompt('Can you guess how many month\'s i\'ve owned and operated Contempo.io?');
+ 
+  lengthGuess = parseInt(lengthGuess);
+
+  if(lengthGuess < careerLength) {
+    alert('I\'m sorry, That guess was too low. Try again');
+  } else if(lengthGuess > careerLength) {
+    alert('I\'m sorry, that guess was too high'); 
+  } else if (lengthGuess === careerLength){
+    alert('you answered correctly!');
+    break;
+  } else {
+    alert('please enter a number and no words');
+  }
+}
