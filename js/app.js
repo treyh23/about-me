e2b93b0c7ca17f101a6aa05b73b01ca728d8f1a1
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-11 14:47:36 -0800

    section one and two complete

--- js/app.js
+++ js/app.js
@@ -2,30 +2,35 @@
 
 var correctGuesses = 0;
 
-var questionOne = prompt('is my hair color blue? Respond with Y or N').toUpperCase();
 
-if(questionOne === 'Y' ) {
-  alert('That is unfortunately not true');
-} else if (questionOne === 'N'){
-  alert('You are correct :)');
-  correctGuesses++;
-} else {
-  alert('you were suppose to answer using Y or N');
+function sectionOne(){
+  var questionOne = prompt('is my hair color blue? Respond with Y or N').toUpperCase();
+  if(questionOne === 'Y' ) {
+    alert('That is unfortunately not true');
+  } else if (questionOne === 'N'){
+    alert('You are correct :)');
+    correctGuesses++;
+  } else {
+    alert('you were suppose to answer using Y or N');
+  }
+  console.log('the users answer is ' + questionOne);
 }
+sectionOne();
 
-console.log('the users answer is ' + questionOne);
+function sectionTwo(){
+  var questionTwo = prompt('is pizza my favorite italian food? - Remember, use Y or N for your answers').toUpperCase();
 
-var questionTwo = prompt('is pizza my favorite italian food? - Remember, use Y or N for your answers').toUpperCase();
-
-if(questionTwo === 'Y') {
-  alert('You are correct! Pizza is my favorite Italian food');
-  correctGuesses++;
-} else if (questionTwo === 'N') {
-  alert('Unfortunate guess - Pizza is my favorite Italian food');
-} else {
-  alert('You entered the wrong answer! Try again');
-};
-console.log(questionTwo + ' is his favorite Italian food!')
+  if(questionTwo === 'Y') {
+    alert('You are correct! Pizza is my favorite Italian food');
+    correctGuesses++;
+  } else if (questionTwo === 'N') {
+    alert('Unfortunate guess - Pizza is my favorite Italian food');
+  } else {
+    alert('You entered the wrong answer! Try again');
+  }
+  console.log(questionTwo + ' is his favorite Italian food!');
+}
+sectionTwo();
 
 var questionThree = prompt('is my name Forrest? - Also, use Y or N for your answer').toUpperCase();
 
